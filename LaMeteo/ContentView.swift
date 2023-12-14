7eabfdd46ae7432d63abec30ff7cb26df126cc00
fetty3 <ovcsinclair@gmail.com>
2023-12-14 20:46:45 +0100

    Initial Commit

---
+++ LaMeteo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LaMeteo
+//
+//  Created by Victor Jegede on 14/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LaMeteo/LaMeteoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaMeteoApp.swift
+//  LaMeteo
+//
+//  Created by Victor Jegede on 14/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct LaMeteoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
